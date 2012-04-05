5fee6aab9ee39ef71f59d48370d2141239bfc2c0
Johannes Wagener <johannes@wagener.cc>
2012-04-05 14:51:56 -0700

    improve and simplify examples, update jquery,

--- examples/examples.js
+++ examples/examples.js
@@ -1,4 +1,12 @@
 $(function(){
   var code = $(".example").html();
   $("#right code").text(code);
+
+  if(window.location.host === "connect.soundcloud.dev"){
+    SC.initialize({
+      client_id:    "694f15bbffd7ae8e6e399f49dd228725",
+      redirect_uri: "http://connect.soundcloud.dev/examples/callback.html",
+      baseUrl:      "http://connect.soundcloud.dev/"
+    });
+  }
 });
