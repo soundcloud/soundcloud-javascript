643e2d0edd910507880db56737b1b347b9ecac57
Johannes Wagener <johannes@wagener.cc>
2012-04-19 16:54:06 -0700

    updating examples to work in development mode

--- examples/examples.js
+++ examples/examples.js
@@ -1,12 +1,21 @@
 $(function(){
-  var code = $(".example").html();
-  $("#right code").text(code);
+  var exampleSource = $(".exampleSource").val();
 
   if(window.location.host === "connect.soundcloud.dev"){
-    SC.initialize({
-      client_id:    "694f15bbffd7ae8e6e399f49dd228725",
-      redirect_uri: "http://connect.soundcloud.dev/examples/callback.html",
-      baseUrl:      "http://connect.soundcloud.dev/"
-    });
+    exampleSource = exampleSource.replace("c202b469a633a7a5b15c9e10b5272b78", "694f15bbffd7ae8e6e399f49dd228725");
+    exampleSource = exampleSource.replace("http://connect.soundcloud.com/examples/callback.html", 'http://connect.soundcloud.dev/examples/callback.html');
+
+    exampleSource = exampleSource.replace("http://connect.soundcloud.com/sdk.js", "/sdk.js");
+
+    exampleSource = exampleSource.replace("SC.initialize({", 'SC.initialize({ baseUrl: "http://connect.soundcloud.dev/",');
+    var developmentMode = true;
+    if(developmentMode) {
+      exampleSource = exampleSource.replace("<script>", "<script>window.SC_DEV_SDK_READY = function(){");
+      exampleSource = exampleSource.replace("\n</script>", "};</script>");
+    }
   }
+
+  $("#right code").text(exampleSource);
+  $(".example").html(exampleSource);
+
 });
