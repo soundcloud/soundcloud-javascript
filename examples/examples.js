bfffa8003d78cbe94d9cf0cd0d0920ef84bf3fca
Johannes Wagener <johannes@wagener.cc>
2012-04-27 15:01:47 +0200

    remove SC_DEV_SDK_READY from examples.js

--- examples/examples.js
+++ examples/examples.js
@@ -5,12 +5,6 @@ $(function(){
     exampleSource = exampleSource.replace("c202b469a633a7a5b15c9e10b5272b78", "694f15bbffd7ae8e6e399f49dd228725");
     exampleSource = exampleSource.replace("http://connect.soundcloud.com/examples/callback.html", 'http://connect.soundcloud.dev/examples/callback.html');
     exampleSource = exampleSource.replace("http://connect.soundcloud.com/sdk.js", "/sdk.js");
-
-    var developmentMode = true;
-    if(developmentMode) {
-      exampleSource = exampleSource.replace("<script>", "<script>window.SC_DEV_SDK_READY = function(){");
-      exampleSource = exampleSource.replace("\n</script>", "};</script>");
-    }
   }
 
   $("#right code").text(exampleSource);
