c925b08b411e46525ac2ea73cfa153b0052a2ade
Tom Jenkinson <tom.jenkinson@soundcloud.com>
2017-07-27 18:24:43 +0200

    update playback library to maestro

---
+++ vendor/playback/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  SCAudio: require('@sc/scaudio'),
+  SCAudioPublicApiStreamURLRetriever: require('@sc/scaudio-public-api-stream-url-retriever'),
+  MaestroCore: require('@sc/maestro-core'),
+  MaestroLoaders: require('@sc/maestro-loaders'),
+  MaestroHTML5Player: require('@sc/maestro-html5-player'),
+  MaestroHLSMSEPlayer: require('@sc/maestro-hls-mse-player')
+};
